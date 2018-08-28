6a909428d90ffd7728617024571aa3a0870f0eec
沈文文 <shenwenwen@gyenno.com>
2018-08-28 16:48:40 +0800

    es6的一些知识点的学习

---
+++ frontNote/js/es6_Rduce.js
@@ -0,0 +1,7 @@
+var values = [1,2,3,4,5];
+values.reduce((pre, cur, index, array) => {
+    console.log(pre);
+    console.log(cur);
+    console.log(index);
+    console.log(array);
+});
--- frontNote/vue/READNE.md
+++ frontNote/vue/READNE.md
@@ -1,1 +1,3 @@
 ##vue及周边篇
+
+1.[使用Intersection Observer API构建无限滚动组件](https://www.w3cplus.com/vue/build-an-infinite-scroll-component-using-intersection-observer-api.html)
